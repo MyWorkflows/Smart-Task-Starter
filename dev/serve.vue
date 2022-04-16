b4858c07db04f493d5fe242fc620514afdedcf4e
Enrico <enricowillemse.was@gmail.com>
2022-04-16 22:29:48 +0800

    Initial

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+import SmartTaskComponent from '@/smart-task-component.vue';
+
+export default defineComponent({
+  name: 'ServeDev',
+  components: {
+    SmartTaskComponent
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <SmartTaskComponent />
+  </div>
+</template>
